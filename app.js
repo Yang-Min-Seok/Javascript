ecc88fa7a7d3d3c13a3be1e2ef5c054cea2388e2
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-12-14 14:17:14 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// const -> 이후에 바꿀 예정이 없는 경우 사용
+const a = 5;
+const b = 2;
+
+// let -> 이후에 바꿀 예정이 있는 경우 사용
+let myName = 'kurooru';
+myName = 'tamama';
+
+// 그러므로, 기본적으로 const사용 후 필요할 때만 let 사용하는쪽으로 코드 짜기
+// Always const sometimes let never var.
