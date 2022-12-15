be0631affc5f02259b682861a5dc2c28ed95fccb
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-12-15 16:45:01 +0900

    1_8_functions part.2

---
+++ app.js
@@ -0,0 +1,8 @@
+const player = {
+    name: 'kurooru',
+    sayHello: function(otherPorsonsName) {
+        console.log(`Hello, ${otherPorsonsName} nice to meet you!`);
+    }
+};
+
+player.sayHello('tamama');
