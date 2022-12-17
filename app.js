63e0cc82cff82a393fb004959f3b52be103be001
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-12-17 19:53:55 +0900

    clean up

--- app.js
+++
@@ -1,9 +0,0 @@
-const age = parseInt(prompt('How old are you?'));
-
-if (isNaN(age)) {
-    // condition == true
-    console.log('Please write a number');
-} else {
-    // condition == false
-    console.log('Thank you for writing your age');
-}
