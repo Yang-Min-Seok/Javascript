95eaf4d8f18f0a63bb04d812b97f0a0c96c17e84
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-12-19 14:43:16 +0900

    clean up

--- app.js
+++
@@ -1,9 +0,0 @@
-const title = document.querySelector('.hello:nth-child(1)')
-
-function handleTitleClick() {
-    // we can change color too!
-    title.style.color='blue';
-}
-
-// add click event
-title.addEventListener("click", handleTitleClick);
