0ce2165f14b365455583994826f72614fb3d1c4e
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-12-19 13:46:12 +0900

    clean up

--- app.js
+++
@@ -1,8 +0,0 @@
-const title = document.getElementById('title');
-// brding every information from Id
-console.dir(title);
-// innerText changes
-title.innerText='Got you!';
-
-console.log(title.id);
-console.log(title.className);
