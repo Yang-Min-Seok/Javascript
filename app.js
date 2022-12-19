0e5bc7187b5f91d73fa3219ef6309b65a26878e6
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-12-19 14:30:13 +0900

    clean up

--- app.js
+++
@@ -1,9 +0,0 @@
-// works like CSS's way of selecting
-// if there are lots of element's,
-// querySelector takes only 'the first one'
-const title = document.querySelector('.hello h1');
-// So if you want take all of them,
-// you should use querySelectorAll
-const title_2 = document.querySelectorAll('.hello h1')
-console.log(title);
-console.log(title_2);
