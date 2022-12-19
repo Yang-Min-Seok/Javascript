3af734d0487b0dec04722bab50085e503aa999d4
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-12-19 13:34:34 +0900

    2_1_HTML in Javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+const title = document.getElementById('title');
+// brding every information from Id
+console.dir(title);
+// innerText changes
+title.innerText='Got you!';
+
+console.log(title.id);
+console.log(title.className);
