c790449f68ad5c1e66fb8ffd53bf1943eaa5aa93
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-12-20 19:35:31 +0900

    2_7 CSS in Javascript part.2

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const h1 = document.querySelector('.hello h1');
 
 function handleTitleClick() {
-    
-    const curr_color = h1.style.color; // can not change
-    let newColor; // can change
-    
-    if (curr_color === 'blue') {
-        newColor = 'tomato';
-    } else {
-        newColor = 'blue';
+
+    const clickedClass = 'clicked';
+
+    if (h1.className == clickedClass) {
+        h1.className = '';
+    } else{
+        h1.className = clickedClass;
     }
     
-    h1.style.color = newColor
 }
 
 h1.addEventListener('click', handleTitleClick);
