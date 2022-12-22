b9717532cadd986417f6bda321fa14d515c1b3ca
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-12-22 15:44:10 +0900

    3_6 Loading Username

--- app.js
+++ app.js
@@ -2,16 +2,32 @@ const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting')
 const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username'
 
 function onLoginSubmit(event) {
     // avoid refreshing (Default)
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem('username', username);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+};
+
+function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
-};
+}
 
 loginForm.addEventListener('submit', onLoginSubmit);
 // JS calls function with some "information(arguments)"
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+    // say hello
+    paintGreetings(savedUsername);
+}
