dd16b45b33d6a75db62394f13434f1b81e64d818
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-12-22 15:07:27 +0900

    3_4 Getting Username

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
-const link =document.querySelector('a');
+const greeting = document.querySelector('#greeting')
+const HIDDEN_CLASSNAME = 'hidden';
 
 function onLoginSubmit(event) {
     // avoid refreshing (Default)
     event.preventDefault();
-    console.log(loginInput.value);
+    loginForm.classList.add('hidden');
+    const username = loginInput.value;
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 };
 
 loginForm.addEventListener('submit', onLoginSubmit);
