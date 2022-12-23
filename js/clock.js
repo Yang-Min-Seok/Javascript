5d8aa5adeaca43b8211696b5b24e318fd0845369
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-12-23 15:20:07 +0900

    4_2 padStart

--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,13 @@
 const clock = document.querySelector('#clock');
 
 function getClock() {
-    const date = new Date()
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const date = new Date();
+    // padStarts, padEnds only works with "strings"
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock() // call getClock immediately
-setInterval(getClock, 1000);
+setInterval(getClock, 1000); // call getClock every 1 sec
