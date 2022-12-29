cc27155fb5792b979f6ce3f8180c6437c13372f0
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-12-29 13:23:11 +0900

    7_1 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,21 @@
+const API_KEY = 'a9476e390a2d512706762d42281be652';
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log(`You live in ${lat}, ${lng}`);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+        const weather = document.querySelector('#weather span:nth-child(1)');
+        const city = document.querySelector('#weather span:nth-child(2)');
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
+
 function onGeoErr() {
     alert(`Can't find you, No weather for you.`)
 }
+
 // call user's location info
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoErr)
